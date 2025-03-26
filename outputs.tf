ef86415d5c81c0e5f00cf79963cd4a18b0c8201a
Matt Scafe <matt.scafe@rackspace.com>
2025-03-26 18:41:47 -0500

    - Added internal_disks to the outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,17 @@
 output "disk_id" {
   value = vcd_vm_internal_disk.int_disk[0].id
 }
+
+output "internal_disks" {
+  description = "Details of internal disks added to the VM"
+  value       = { for idx, disk in vcd_vm_internal_disk.int_disk : idx => {
+    vm_name         = disk.vm_name
+    size_in_mb      = disk.size_in_mb
+    bus_number      = disk.bus_number
+    unit_number     = disk.unit_number
+    bus_type        = disk.bus_type
+    iops            = disk.iops
+    storage_profile = disk.storage_profile
+  } }
+}
+
