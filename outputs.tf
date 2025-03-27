c055dfde554b9841e9207b6864b17f9cc28fe116
Matt Scafe <matt.scafe@rackspace.com>
2025-03-26 19:46:04 -0500

    - Fixed error on outputs.tf file after removing count function

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,16 @@
 output "disk_id" {
-  value = vcd_vm_internal_disk.int_disk[0].id
+  value = vcd_vm_internal_disk.int_disk.id
 }
 
 output "internal_disks" {
-  description = "Details of internal disks added to the VM"
-  value       = { for idx, disk in vcd_vm_internal_disk.int_disk : idx => {
-    vm_name         = disk.vm_name
-    size_in_mb      = disk.size_in_mb
-    bus_number      = disk.bus_number
-    unit_number     = disk.unit_number
-    bus_type        = disk.bus_type
-    iops            = disk.iops
-    storage_profile = disk.storage_profile
-  } }
+  description = "Details of internal disk added to the VM"
+  value = {
+    vm_name         = vcd_vm_internal_disk.int_disk.vm_name
+    size_in_mb      = vcd_vm_internal_disk.int_disk.size_in_mb
+    bus_number      = vcd_vm_internal_disk.int_disk.bus_number
+    unit_number     = vcd_vm_internal_disk.int_disk.unit_number
+    bus_type        = vcd_vm_internal_disk.int_disk.bus_type
+    iops            = vcd_vm_internal_disk.int_disk.iops
+    storage_profile = vcd_vm_internal_disk.int_disk.storage_profile
+  }
 }
-
